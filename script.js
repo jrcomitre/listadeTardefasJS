4910ab232c0be2a734b66a647dfd8d0471737c88
jrcomitre <gcomitrejr@gmail.com>
2023-01-03 12:05:05 -0300

    Iniciando a aplicacao

---
+++ script.js
@@ -0,0 +1,26 @@
+// Referenciar o input
+let input = document.querySelector('input[name=tarefa');
+
+
+// Referenciar o Button
+let btn = document.querySelector('#botao');
+
+
+// Referenciar a lista Adicionar tarefas
+let lista = document.querySelector('#lista');
+
+let tarefas = [
+    'Jogar GTA',
+    'Estudar Python',
+    'Estudar React',
+    'Estudar Inglês',
+    'Assistir Filme'
+];
+
+/*
+<li class="list-group-item list-group-item-action">Jogar GTA</li>
+<li class="list-group-item list-group-item-action">Estudar Python</li>
+<li class="list-group-item list-group-item-action">Estudar React</li>
+<li class="list-group-item list-group-item-action">Estudar Inglês</li>
+<li class="list-group-item list-group-item-action">Assistir Filme</li>
+*/
